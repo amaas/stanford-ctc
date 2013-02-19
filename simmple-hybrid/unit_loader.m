7d66f11174e9055955f1bce8c3deb8ef26fc4d2d
awni <awni.hannun@gmail.com>
2013-02-18 20:51:03 -0800

    changed load_kaldi_dat.m to just load presaved files with kaldi data.  now just give the loader an absolute path to where the data is stored and will load.  made some changes to feat-ali-write to write utterance keys and sizes as well.  this will be useful in writing loglikelihoods for decoding

--- simmple-hybrid/unit_loader.m
+++ simmple-hybrid/unit_loader.m
@@ -1,8 +1,17 @@
 % AMAAS test setup for loader script
 addpath ../util/
-git_root = '/afs/cs.stanford.edu/u/amaas/scratch/audio/kaldi-stanford/';
-fdir = '/afs/cs.stanford.edu/u/amaas/scratch/audio/kaldi-stanford/kaldi-trunk/egs/swbd/s5/data-fmllr/';
-alidir = '/afs/cs.stanford.edu/u/amaas/scratch/audio/kaldi-stanford/kaldi-trunk/egs/swbd/s5/exp/tri4a_ali_100k_nodup/';
+%amaas_git_root = '/afs/cs.stanford.edu/u/amaas/scratch/audio/kaldi-stanford/';
+git_root = '/scail/group/deeplearning/speech/awni/kaldi-stanford/';
 
-% try it
-[f, a] = load_kaldi_data(git_root, fdir, alidir);
+%fdir = '/afs/cs.stanford.edu/u/amaas/scratch/audio/kaldi-stanford/kaldi-trunk/egs/swbd/s5/data-fmllr/';
+%alidir = '/afs/cs.stanford.edu/u/amaas/scratch/audio/kaldi-stanford/kaldi-trunk/egs/swbd/s5/exp/tri4a_ali_100k_nodup/';
+
+dat_dir = [git_root 'kaldi-trunk/egs/swbd/s5/exp/nn_data/'];
+
+
+[f, a, utt_dat] = load_kaldi_data(dat_dir);
+
+disp(size(f))
+disp(size(a))
+disp(size(utt_dat.keys))
+disp(size(utt_dat.sizes))
