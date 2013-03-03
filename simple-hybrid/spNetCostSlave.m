da9a29e27dc0242d814ee7cac04f645e9bf33ab2
Andrew Maas <andrew.maas@gmail.com>
2013-03-02 16:49:14 -0800

    slave objective now supports running full objective on cpu. could be a problem when model or data fills card

--- simple-hybrid/spNetCostSlave.m
+++ simple-hybrid/spNetCostSlave.m
@@ -10,12 +10,15 @@ function [ cost, grad, numCorrect, numExamples, ceCost, wCost] = spNetCostSlave(
 cost=0;
 wCost = 0;
 grad=[];
-numCorrect = 0;
 %% reshape into network
 stack = params2stack(theta, eI);
 numHidden = numel(eI.layerSizes) - 1;
 hAct = cell(numHidden+1, 1);
 gradStack = cell(numHidden+1, 1);
+%% setup GPU vars
+if (eI.useGpu)
+    data = gdouble(data);
+end;
 %% forward prop
 % logistic layers
 for i = 1 : numHidden
@@ -25,7 +28,7 @@ for i = 1 : numHidden
         hAct{i} = stack{i}.W * hAct{i-1};
     end;
     % bias term
-    hAct{i}= bsxfun(@plus, hAct{i}, stack{i}.b);        
+    hAct{i}= bsxfun(@plus, hAct{i}, stack{i}.b);      
     %% nonlinearity
     if strcmpi(eI.activationFn,'tanh')
         hAct{i} = tanh(hAct{i});
@@ -36,27 +39,29 @@ for i = 1 : numHidden
     end;
 end
 %% softmax layer
-hAct{end} = exp(bsxfun(@plus,stack{end}.W * hAct{end-1}, stack{end}.b));
+hAct{end} = exp(bsxfun(@plus, stack{end}.W * hAct{end-1}, stack{end}.b));
 hAct{end} = bsxfun(@rdivide,hAct{end},sum(hAct{end}));
 
 %% get labels as matrix
 [n,m] = size(data);
-groundTruth = sparse(labels, 1:m, 1,eI.layerSizes(end),m);    
+groundTruth = sparse(labels, 1:m, 1,eI.layerSizes(end),m);
+if eI.useGpu
+    groundTruth = gdouble(full(groundTruth));
+end;
 %% compute accuracy
 [~,pred] = max(hAct{end});
 % accList = [accList; mean(pred'==curLabels)];
 % numExList = [numExList; m];
-numCorrect = sum(pred'==labels);
+numCorrect = double(sum(pred'==labels));
 
 %% compute cost
-cost = cost - sum(sum(log(hAct{end}) .* groundTruth));
+cost = cost - sum(sum(log(hAct{end}) .* (groundTruth)));
 
 %% compute gradient for SM layer
 delta = hAct{end}-groundTruth;
 gradStack{end}.W = delta*hAct{end-1}';
 gradStack{end}.b = sum(delta, 2);
-% prop error through SM layer    
-% TODO transposing this could be expensive
+% prop error through SM layer
 delta = stack{end}.W'*delta;
 %% gradient for hidden layers
 for i = numHidden:-1:1
@@ -79,16 +84,21 @@ for i = numHidden:-1:1
     gradStack{i}.b = sum(delta,2);
     % prop through weights for lower layer
     if i > 1
-        delta = stack{i}.W'*delta;
+        delta = stack{i}.W' * delta;
     end;
 end;
 %% add weight norm penalties for non-bias terms
 numExamples = size(data,2);
 % logistic layers
 for i=1:numHidden+1
-    wCost = wCost + sum(stack{i}.W(:).^2);
+    % GPU and CPU versions because norm is slow on cpu
+    if eI.useGpu
+        wCost = wCost + norm(stack{i}.W,'fro')^2;
+    else
+        wCost = wCost + sum(stack{i}.W(:).^2);
+    end;
     gradStack{i}.W =  gradStack{i}.W  + ...
-        2 * numExamples * eI.lambda * stack{i}.W;    
+        2 * numExamples * eI.lambda * stack{i}.W;
 end
 % scale wCost once it contains all weight norms
 wCost = wCost * eI.lambda * numExamples;
@@ -97,6 +107,11 @@ wCost = wCost * eI.lambda * numExamples;
 %% compute final cost
 ceCost = cost;
 cost = cost + wCost;
+%% return from gpu. should be no-op on CPU
+grad = double(grad);
+cost = double(cost);
+ceCost = double(ceCost);
+wCost = double(wCost);
 end
 
 
