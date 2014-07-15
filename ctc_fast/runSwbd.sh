126f75f3b8808d15c1f675169eee0308f9ec030e
awni <awni.hannun@gmail.com>
2014-07-15 07:24:26 -0700

    best birnn recipe so far in runswbd.sh

--- ctc_fast/runSwbd.sh
+++ ctc_fast/runSwbd.sh
@@ -5,18 +5,18 @@ set -x
 
 momentum=.95 
 epochs=20
-layerSize=2000
+layerSize=1824
 numLayers=5
 maxBatch=1500
-step=5e-5
+step=1e-5
 anneal=1.3
 deviceId=0
-temporalLayer=-1
+temporalLayer=3
 
-outfile="models/swbd_layers_${numLayers}_${layerSize}_step_${step}_mom_${momentum}_anneal_${anneal}.bin"
+outfile="models/swbd_layers_${numLayers}_${layerSize}_bitemporal_${temporalLayer}_step_${step}_mom_${momentum}_anneal_${anneal}.bin"
 echo $outfile
 python runNNet.py --layerSize $layerSize --numLayers $numLayers \
   --step $step --epochs $epochs --momentum $momentum --anneal $anneal \
-  --outputDim 35 --inputDim $((41*15)) --rawDim $((41*15))  --numFiles 384 \
+  --outputDim 35 --inputDim $((41*15)) --rawDim $((41*15))  --numFiles 384\
   --dataDir /scail/group/deeplearning/speech/awni/kaldi-stanford/kaldi-trunk/egs/swbd/s5b/exp/train_ctc/ \
   --maxUttLen $maxBatch --outFile $outfile --deviceId $deviceId --temporalLayer $temporalLayer
