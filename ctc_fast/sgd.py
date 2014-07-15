80712a2115eaa8e6d2052de7d4a274d2e4323445
awni <awni.hannun@gmail.com>
2014-07-15 07:11:35 -0700

    gradient clipping in sgd

--- ctc_fast/sgd.py
+++ ctc_fast/sgd.py
@@ -2,18 +2,30 @@ import numpy as np
 import cudamat as cm
 import pickle
 import random
-
+import pdb
 
 class SGD:
 
-    def __init__(self,model,maxBatch,alpha=1e-2,momentum=0.9):
+    def __init__(self,model,maxBatch,alpha=1e-2,optimizer='nesterov',
+                 momentum=0.9):
         self.model = model
         self.maxBatch = maxBatch
         self.it = 0
         self.momentum = momentum # momentum
         self.alpha = alpha # learning rate
-        self.velocity = [[cm.CUDAMatrix(np.zeros(w.shape)),cm.CUDAMatrix(np.zeros(b.shape))] 
-                          for w,b in self.model.stack]
+        self.optimizer = optimizer
+        self.maxGNorm = 1500 # gradient clip norm value
+
+        if self.optimizer == 'nesterov':
+            self.velocity = [[cm.CUDAMatrix(np.zeros(w.shape)),
+                              cm.CUDAMatrix(np.zeros(b.shape))] 
+                              for w,b in self.model.stack]
+        elif self.optimizer == 'adagrad':
+            epsilon = 0.0
+            self.gradt = [[cm.CUDAMatrix(epsilon+np.zeros(w.shape)),
+                           cm.CUDAMatrix(epsilon+np.zeros(b.shape))] 
+                           for w,b in self.model.stack]
+
 	self.costt = []
 	self.expcost = []
 
@@ -38,18 +50,36 @@ class SGD:
 
             mb_data = data_dict[k]
             if mb_data.shape[1] > self.maxBatch:
-                print "SKIPPING utt exceeds batch length. (Utterance length %d)"%mb_data.shape[1]
+                print ("SKIPPING utt exceeds batch length "
+                       "(Utterance length %d).")%mb_data.shape[1]
                 continue
 
-            # w = w+mom*velocity (evaluate gradient at future point)
-            self.model.updateParams(mom,self.velocity)
             mb_labels = np.array(alis[k],dtype=np.int32)
+
+            if mb_data.shape[1] < mb_labels.shape[0]:
+                print ("SKIPPING utt frames less than label length "
+                       "(Utterance length %d, Num Labels %d)."
+                       "")%(mb_data.shape[1],mb_labels.shape[0])
+                continue
+
+
+            if self.optimizer == 'nesterov':
+                # w = w+mom*velocity (evaluate gradient at future point)
+                self.model.updateParams(mom,self.velocity)
 
             cost,grad,skip = self.model.costAndGrad(mb_data,mb_labels)
 
-	    # undo update
-            # w = w-mom*velocity
-            self.model.updateParams(-mom,self.velocity)
+            if self.optimizer == 'nesterov':
+                # undo update
+                # w = w-mom*velocity
+                self.model.updateParams(-mom,self.velocity)
+
+            # Compute norm of all parameters as one vector
+            gnorm = 0.0
+            for dw,db in grad:
+                gnorm += dw.euclid_norm()**2
+                gnorm += db.euclid_norm()**2
+            gnorm = np.sqrt(gnorm)
 
 	    if skip:
 		print "SKIPPING: Key=%s, Cost=%f, SeqLen=%d, NumFrames=%d."%(k,
@@ -65,21 +95,43 @@ class SGD:
                 self.costt.append(cost)
 
             # velocity = mom*velocity - alpha*grad
-            for vs,gs in zip(self.velocity,grad):
-                vw,vb = vs 
-                dw,db = gs
-                vw.mult(mom)
-                vb.mult(mom)
-                vw.add_mult(dw,alpha=-self.alpha)
-                vb.add_mult(db,alpha=-self.alpha)
+            if self.optimizer == 'nesterov':
+                alph = self.alpha
+                if gnorm > self.maxGNorm:
+                    alph *= (self.maxGNorm/gnorm)
+                for vs,gs in zip(self.velocity,grad):
+                    vw,vb = vs 
+                    dw,db = gs
+                    vw.mult(mom)
+                    vb.mult(mom)
+                    vw.add_mult(dw,alpha=-alph)
+                    vb.add_mult(db,alpha=-alph)
+                update = self.velocity
+                scale = 1.0
 
-            update = self.velocity
-            scale = 1.0
+            elif self.optimizer == 'adagrad':
+                delta = 1e-10
+                for gts,gs in zip(self.gradt,grad):
+                    dwt,dbt = gts 
+                    dw,db = gs
+                    gamma = 1. - 1./(1e-2*self.it+1)
+                    cm.add_pow(dwt,dw,2,alpha=gamma,target=dwt)
+                    cm.add_pow(dbt,db,2,alpha=gamma,target=dbt)
+                    dwt.add(delta,target=dwt)
+                    dbt.add(delta,target=dbt)
+                    cm.mult_pow(dw,dwt,-0.5,target=dw)
+                    cm.mult_pow(db,dbt,-0.5,target=db)
+                    dwt.add(-delta,target=dwt)
+                    dbt.add(-delta,target=dbt)
+                update = grad
+                scale = -self.alpha
 
 	    # update params
 	    self.model.updateParams(scale,update)
 
             if self.it%1 == 0:
-                print "Iter %d : Cost=%.4f, ExpCost=%.4f, SeqLen=%d, NumFrames=%d."%(self.it,
-                        cost,self.expcost[-1],mb_labels.shape[0],mb_data.shape[1])
+                print ("Iter %d : Cost=%.4f, ExpCost=%.4f, GradNorm=%.4f, "
+                       "SeqLen=%d, NumFrames=%d.")%(self.it,cost,
+                       self.expcost[-1],gnorm,mb_labels.shape[0],
+                       mb_data.shape[1])
         
