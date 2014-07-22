bugfix with wrong order likelihood writing

ctc_fast/brnnet.py
@@ -162,7 +162,7 @@ class NNet:
         self.probs.copy_to_host()
         if not self.train: 
             if returnProbs:
-                return np.ascontiguousarray(self.probs.numpy_array)
+                return self.probs.numpy_array
             else:
                 return ctc.decode_best_path(self.probs.numpy_array.astype(np.float64))
 

ctc_fast/writeLikelihoods.py
@@ -37,10 +37,10 @@ def writeLogLikes(loader,nn,outDir,fn):
             "Need larger max utt length."
         writeUttHeader(fid,k,sizes[i],nn.outputDim)
         probs = nn.costAndGrad(data_dict[k],returnProbs=True)
-        assert not np.isfortran(probs) and probs.dtype==np.float32,\
-            "Probs array malformed."
+        assert probs.dtype==np.float32,"Probs array malformed."
+        assert probs.shape[0]==nn.outputDim,"Probs dimensions mismatch."
         probs = np.log(probs)
-        probs.tofile(fid)
+        probs.T.tofile(fid)
 
     print "Done with file %d"%fn
     fid.close()