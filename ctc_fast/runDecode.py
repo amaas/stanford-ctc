use dnn, contractions, fix wer computation

ctc_fast/runDecode.py
@@ -6,20 +6,16 @@ import cPickle as pickle
 from collections import defaultdict
 from joblib import Parallel, delayed
 from decoder.decoder_config import SPACE, SCAIL_DATA_DIR,\
-        INPUT_DIM, RAW_DIM, DATASET, DATA_SUBSET, LM_ARPA_FILE
+        INPUT_DIM, RAW_DIM, DATASET, DATA_SUBSET
 from cluster.config import NUM_CPUS, CLUSTER_DIR, PYTHON_CMD
 from cluster.utils import get_free_nodes, run_cpu_job
 from decoder.decoder_utils import decode
-#import kenlm
-from srilm import LM
 if DATA_SUBSET == 'eval2000':
     from decoder.decoder_config import SWBD_SUBSET
-
-#from nclm import NCLM, NCLMHyperparams
-from rnn import RNN, RNNHyperparams
+from model_utils import get_model_class_and_params
 from run_utils import load_config
 from optimizer import OptimizerHyperparams
-from char_corpus import CharCorpus
+from errorAnalysis import replace_contractions
 
 '''
 Take probs outputted by runNNet.py forward props and run decoding
@@ -54,7 +50,7 @@ def decode_utterance(k, probs, labels, phone_map, lm=None):
     probs = probs.astype(np.float64)
 
     hyp, hypscore, truescore = decode(probs,
-            alpha=1.0, beta=1.5, beam=40, method='clm2', clm=lm)
+            alpha=3.0, beta=1.5, beam=200, method='clm2', clm=lm)
 
     return (hyp, ref, hypscore, truescore)
 
@@ -74,20 +70,15 @@ def runSeq(opts):
     numphones = list()
     subsets = list()
 
-    #cfg_file = '/afs/cs.stanford.edu/u/zxie/scail_data_zxie/nclm_models/10/cfg.json'
-    cfg_file = '/deep/u/zxie/rnnlm/1/cfg.json'
+    cfg_file = '/deep/u/zxie/dnn/3/cfg.json'
     cfg = load_config(cfg_file)
-    #model_hps = NCLMHyperparams()
-    model_hps = RNNHyperparams()
+    model_class, model_hps = get_model_class_and_params('dnn')
     opt_hps = OptimizerHyperparams()
     model_hps.set_from_dict(cfg)
     opt_hps.set_from_dict(cfg)
-    dataset = CharCorpus(model_hps.T, model_hps.batch_size, subset='dev')
 
-    #clm = NCLM(dataset, model_hps, opt_hps, train=False, opt='nag')
-    clm = RNN(dataset, model_hps, opt_hps, train=False, opt='nag')
-    #with open('/scail/data/group/deeplearning/u/zxie/nclm_models/10/params.pk', 'rb') as fin:
-    with open('/deep/u/zxie/rnnlm/1/params.pk', 'rb') as fin:
+    clm = model_class(None, model_hps, opt_hps, train=False, opt='nag')
+    with open('/deep/u/zxie/dnn/3/params.pk', 'rb') as fin:
         clm.from_file(fin)
     #clm = None
 
@@ -115,6 +106,7 @@ def runSeq(opts):
             if hypscore is None:
                 hypscore = 0.0
             hyp = [phone_map[h] for h in hyp]
+            hyp = replace_contractions(hyp)
             fid.write(k + ' ' + ' '.join(hyp) + '\n')
 
             hyps.append(hyp)

ctc_fast/swbd-utils/errorAnalysis.py
@@ -54,7 +54,51 @@ def disp_err_corr(hyp_corr, ref_corr):
     print ref_str
 
 
-def compute_and_display_stats(hyps, refs, hypscores, refscores, numphones, subsets, subset=None):
+def replace_contractions(utt):
+    if len(utt):
+        while len(utt) and utt[-1] == '[space]':
+            utt = utt[:-1]
+        while len(utt) and utt[0] == '[space]':
+            utt = utt[1:]
+
+    # TODO Replace in training text instead
+    utt_str = ''.join([c if c != '[space]' else ' ' for c in utt])
+
+    utt_str = utt_str.replace('can\'t', 'cannot')
+    utt_str = utt_str.replace('let\'s', 'let us')
+
+    # Possessive vs " is"
+    utt_str = utt_str.replace('ere\'s', 'ere is')
+    utt_str = utt_str.replace('that\'s', 'that is')
+    utt_str = utt_str.replace('he\'s', 'he is')
+    utt_str = utt_str.replace('it\'s', 'it is')
+    utt_str = utt_str.replace('how\'s', 'how is')
+    utt_str = utt_str.replace('what\'s', 'what is')
+    utt_str = utt_str.replace('when\'s', 'when is')
+    utt_str = utt_str.replace('why\'s', 'why is')
+
+    utt_str = utt_str.replace('\'re', ' are')
+
+    utt_str = utt_str.replace('i\'m', 'i am')
+    utt_str = utt_str.replace('\'ll', ' will')
+    utt_str = utt_str.replace('\'d', ' would')  # had / would ambiguity
+    utt_str = utt_str.replace('n\'t', ' not')
+    utt_str = utt_str.replace('\'ve', ' have')
+
+    ''' Different between swbd and callhm?
+    utt_str = utt_str.replace('uh-huh', 'um-hum')
+    utt_str = utt_str.replace('uh-hum', 'um-hum')
+    '''
+    utt_str = utt_str.replace(' uh', '')
+    utt_str = utt_str.replace(' um', '')
+    utt_str = utt_str.replace('uh ', '')
+    utt_str = utt_str.replace('um ', '')
+
+    utt = [c if c != ' ' else '[space]' for c in list(utt_str)]
+    return utt
+
+
+def compute_and_display_stats(hyps, refs, hypscores, refscores, numphones, subsets, subset=None, display=False):
     # Filter by subset
     if subset:
         print 'USING SUBSET: %s' % subset
@@ -72,19 +116,19 @@ def compute_and_display_stats(hyps, refs, hypscores, refscores, numphones, subse
     hyp_lens = [len(s) for s in hyps]
     ref_lens = [len(s) for s in refs]
 
-    max_hyp_len = max(hyp_lens)
+    max_hyp_len = max([len(hyp) for hyp in hyps])
     tot_errs_by_pos = np.zeros(max_hyp_len)
     counts_by_pos = np.zeros(max_hyp_len, dtype=np.int32)
 
     tot_dist = tot_eq = tot_ins = tot_dels = tot_subs = 0.0
     num_sents_correct = 0
     correct_sents_len = 0
-    frac_wrong = list()
 
     #pbar = ProgressBar(maxval=len(hyps)).start()
 
     k = 0
     for (hyp, ref, hypscore, refscore) in zip(hyps, refs, hypscores, refscores):
+        #hyp = replace_contractions(hyp)
         dist, eq, ins, dels, subs, errs_by_pos, hyp_corr, ref_corr = ed(hyp, ref)
         tot_eq += eq
         tot_ins += ins
@@ -99,11 +143,10 @@ def compute_and_display_stats(hyps, refs, hypscores, refscores, numphones, subse
             num_sents_correct += 1
             correct_sents_len += len(ref)
         tot_dist += dist
-        # NOTE Smoothing
-        frac_wrong.append(dist / float(len(ref) + 1.0))
 
-        disp_err_corr(hyp_corr, ref_corr)
-        print
+        if display:
+            disp_err_corr(hyp_corr, ref_corr)
+            print
 
     '''
     Display aggregate stats
@@ -116,9 +159,9 @@ def compute_and_display_stats(hyps, refs, hypscores, refscores, numphones, subse
     print 'avg ref score: %f' % (sum(refscores) / len(refscores))
     print 'avg hyp score: %f' % (sum(hypscores) / len(hypscores))
 
-    tot_hyp_len = float(np.sum(hyp_lens))
+    tot_comp_len = float(np.sum([max(h, r) for (h, r) in zip(hyp_lens, ref_lens)]))
     print 'frac eq: %f ins: %f del: %f sub: %f' %\
-        tuple(np.array([tot_eq, tot_ins, tot_dels, tot_subs]) / tot_hyp_len)
+        tuple(np.array([tot_eq, tot_ins, tot_dels, tot_subs]) / tot_comp_len)
 
     print 'CER: %f' % (100.0 * tot_dist / np.sum(numphones))
 
@@ -144,12 +187,13 @@ def main(args):
     subsets = None
     fid.close()
 
-    compute_and_display_stats(hyps, refs, hypscores, refscores, numphones, subsets, subset=None)
+    compute_and_display_stats(hyps, refs, hypscores, refscores, numphones, subsets, subset=None, display=args.display)
 
 
 if __name__ == '__main__':
     import argparse
     parser = argparse.ArgumentParser()
     parser.add_argument('pk_file', default='hyp.pk', help='Pickle file with data')
+    parser.add_argument('--display', action='store_true')
     args = parser.parse_args()
     main(args)